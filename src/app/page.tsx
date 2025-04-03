2dbe7eeed03ec6c9ad5a2eb516cbc1fe24d252c1
Ebha-Mollick <ebhamollick@gmail.com>
2025-04-03 10:58:49 +0530

    added instructors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/ui/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+import Instructors from "@/components/Instructors";
 
 
 import Image from "next/image";
@@ -15,7 +16,8 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
-
+      <UpcomingWebinar/>
+      <Instructors />
     </main>
   );
 }
